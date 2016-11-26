48cf3a31a2e253fa3fafb55715fd729569f4bc37
Ramiro Lopez <Ramiro@Ramiros-MacBook-Pro.local>
2016-11-25 22:45:36 -0800

    CleanUP

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# week-4-game
-Homework # 4 for Week # 4 (UCLACD)
+# Pokemon RPG - A web based Javascript hangman game.  
+
+A Web based Javascript/ HTML5/ CSS Hangman application that randomly generates a word for the user to guess.
+
+## Live Demo
+
+[See Pokemon RPG Live](https://pokemonrpgweek4.herokuapp.com/)
+
+![Pokemon RPG](assets/images/PokemonRPG.gif)
+
+
+## Usage
+
+1. Clone repo
+2. npm install
+3. cd PokemonRPG
+4. open index.html in any browser
+5. Follow game instruction and choose a character. 
+6. ENJOY! 
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## Tech Used 
+
+* HTML5 
+* CSS 
+* JAVASCRIPT 
+* CSS 
+
+## License
+
+N/A
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,50 +9,50 @@ var baseAttack;
 var defendersAlive;
 
 
-function initializeGame(){
+function initializeGame() {
     umbreon = {
-    id: 0,
-    name: 'umbreon',
-    hp: 150,
-    attack: 10
-};
+        id: 0,
+        name: 'umbreon',
+        hp: 150,
+        attack: 10
+    };
 
-doubleblade = {
-    id: 1,
-    name: 'doubleblade',
-    hp: 125,
-    attack: 10
-};
+    doubleblade = {
+        id: 1,
+        name: 'doubleblade',
+        hp: 125,
+        attack: 10
+    };
 
-gastly = {
-    id: 2,
-    name: 'gastly',
-    hp: 80,
-    attack: 10
-};
+    gastly = {
+        id: 2,
+        name: 'gastly',
+        hp: 80,
+        attack: 10
+    };
 
-reshiram = {
-    id: 3,
-    name: 'reshiram',
-    hp: 115,
-    attack: 15
-};
+    reshiram = {
+        id: 3,
+        name: 'reshiram',
+        hp: 115,
+        attack: 15
+    };
 
-defendersAlive = 3;
-characters = [umbreon, doubleblade, gastly, reshiram];
-chosenEnemies = [];
-currentCharacter = undefined;
-defender = undefined;
-baseAttack = undefined;
+    defendersAlive = 3;
+    characters = [umbreon, doubleblade, gastly, reshiram];
+    chosenEnemies = [];
+    currentCharacter = undefined;
+    defender = undefined;
+    baseAttack = undefined;
 
-$("#init-shown > div").fadeIn("slow");
-$("#init-hidden > div").fadeOut("slow");
-$("#characterHealth").text("Character Health: 0");
-$("#enemyHealth").text("Enemy Health: 0");
+    $("#init-shown > div").fadeIn("slow");
+    $("#init-hidden > div").fadeOut("slow");
+    $("#characterHealth").text("Character Health: 0");
+    $("#enemyHealth").text("Enemy Health: 0");
 }
 
 initializeGame();
-			
+
 // Player chooses his character and hides the remaing 3 which become enemies
 function characterClicked(characterIndex) {
     for (var i = 0; i < 4; i++) {
@@ -86,8 +86,8 @@ function enemyClicked(characterIndex) {
                 $("#defender" + i).fadeIn("slow");
                 $("#enemies" + characterIndex).fadeOut("slow");
                 defender = characters[characterIndex];
-                    $("#enemyHealth").text("Enemy Health: " + defender.hp);
-	
+                $("#enemyHealth").text("Enemy Health: " + defender.hp);
+
             }
         }
 
@@ -96,11 +96,10 @@ function enemyClicked(characterIndex) {
 }
 
 //This is to make the attack button work
-$("#attackbtn").click(function(){
-	if (defender !== undefined){
-	attack();
-}
-    else {
+$("#attackbtn").click(function() {
+    if (defender !== undefined) {
+        attack();
+    } else {
         alert("PLEASE CHOOSE A DEFENDER");
     }
 })
@@ -109,75 +108,31 @@ $("#attackbtn").click(function(){
 //attack with his attack value.
 
 function attack() {
-	currentCharacter.hp -= defender.attack;
-	//console.log(currentCharacter.hp);
+    currentCharacter.hp -= defender.attack;
+    //console.log(currentCharacter.hp);
     defender.hp -= currentCharacter.attack;
-	//console.log(defender.hp);
+    //console.log(defender.hp);
     currentCharacter.attack += baseAttack;
 
-	$("#enemyHealth").text("Enemy Health: " + defender.hp);
+    $("#enemyHealth").text("Enemy Health: " + defender.hp);
     $("#characterHealth").text("Character Health: " + currentCharacter.hp);
     //console.log(currentCharacter.attack);
     //Add function if enemy goes below 0.
 
 
 
-    if (currentCharacter.hp <= 0){
-		alert("YOU LOSE-GAME OVER!");
+    if (currentCharacter.hp <= 0) {
+        alert("YOU LOSE-GAME OVER!");
         initializeGame();
-	} 
-    else if(defender.hp <= 0){
-	   $("#defender" + defender.id).fadeOut("slow");
-       $("#enemyHealth").text("Enemy Health: 0");
-       defendersAlive --;
-       defender = undefined;
-   }
+    } else if (defender.hp <= 0) {
+        $("#defender" + defender.id).fadeOut("slow");
+        $("#enemyHealth").text("Enemy Health: 0");
+        defendersAlive--;
+        defender = undefined;
+    }
 
-   if(defendersAlive == 0){
+    if (defendersAlive == 0) {
         alert("YOU WIN!!!!!");
         initializeGame();
     }
 }
-
-
-
-
-
-
-
-
-	// This will take my character and subtract my attack power from defneders hp. 
-	// Then defender will do the same to me until one of us reaches <= 0 HP.
-	// If i die the game ends 
-	// If defender dies I will chose the next enemy and fight him with my same hp until one
-	// of us dies. if I die game is over if he dies i will fight th next defender if any. 
-	// Keeping track of who is alive who is dead and my hp and my attach goes up by some amount.
-	// If I win Alert you win if i lose alert GAME OVER. 
-	// Set a reset or play again fucntion or button. 
-	// check each time we hit attack if i am at 0 or if defender is at 0. 
-
-    // class Character {
-    //          "peter" 25 10
-    // constructor(name, hp, attack) {
-    //      this.name = name;
-    //      this.hp = hp;
-    //      this.attack = attack;
-    //  }   
-    // }
-
-    // var c1 = new Character("peter", 25, 10);
-
-    // console.log(c1.name); //peter
-
-
-
-    // function someFunction(characterIndex) {
-    //  //characterindex = 3
-    //  for (var ; i < )
-
-    // }
-
-    /*psuedo code _______________________________________________
-    //Have 4 characters in the initial div where they are clickable for player to chose 1 character.
-    //once the player chooses his chratcer it stays in the CYC div and the remaing 3 become enemies and move to EATA
-    //*/
